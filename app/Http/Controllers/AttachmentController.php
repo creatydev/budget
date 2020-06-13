8ae93430faceba35a33eb41f37175e5051acbd6e
Daniël van Drunen <daniel@pixely.me>
2020-06-13 21:32:26 +0200

    Store uploaded attachments

--- app/Http/Controllers/AttachmentController.php
+++ app/Http/Controllers/AttachmentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Repositories\AttachmentRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class AttachmentController extends Controller
 {
@@ -18,13 +20,17 @@ class AttachmentController extends Controller
     {
         $request->validate([
             'transaction_type' => 'required|in:earning,spending',
-            'transaction_id' => 'required'
+            'transaction_id' => 'required',
+            'yeet' => 'required'
         ]);
+
+        $fileName = Str::random(20) . '.' . $request->file('yeet')->extension();
+        $filePath = $request->file('yeet')->storeAs('attachments', $fileName);
 
         $transactionType = $request->transaction_type;
         $transactionId = $request->transaction_id;
 
-        $this->attachmentRepository->create($transactionType, $transactionId);
+        $this->attachmentRepository->create($transactionType, $transactionId, $filePath);
 
         return redirect('/' . $transactionType . 's/' . $transactionId);
     }
--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Attachment extends Model
 {
@@ -11,4 +12,18 @@ class Attachment extends Model
         'transaction_id',
         'file_path'
     ];
+
+    // Accessors
+    public function getYeetAttribute()
+    {
+        $file = Storage::get($this->file_path);
+
+        if (!$file) {
+            return null;
+        }
+
+        $type = pathinfo($this->file_path, PATHINFO_EXTENSION);
+
+        return 'data:image/' . $type . ';base64,' . base64_encode($file);
+    }
 }
